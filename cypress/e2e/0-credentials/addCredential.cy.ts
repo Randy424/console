6a86a299fb1518470591737e07403ef9b2a789ea
randybrunopiverger <rbrunopi@redhat.com>
2023-08-03 13:40:16 -0700

    enable testing
    Signed-off-by: randybrunopiverger <rbrunopi@redhat.com>

--- cypress/e2e/0-credentials/addCredential.cy.ts
+++ cypress/e2e/0-credentials/addCredential.cy.ts
@@ -5,20 +5,20 @@ describe('example to-do app', () => {
   // TODO: VALIDATE THAT THE NECESSARY FIXTURES ARE PRESENT
 
   // TODO: add tags
-  // it('CLC: Traverse to create credential page from empty state credential page', () => {
-  //   cy.visit('/multicloud/credentials')
-  //   cy.get('a').contains('Add credential').should('have.attr', 'aria-disabled', 'false').click()
-  //   cy.location('href').should('include', '/credentials/create')
-  // })
+  it('CLC: Traverse to create credential page from empty state credential page', () => {
+    cy.visit('/multicloud/credentials')
+    cy.get('a').contains('Add credential').should('have.attr', 'aria-disabled', 'false').click()
+    cy.location('href').should('include', '/credentials/create')
+  })
 
-  // it('CLC: Traverse to create credential page from populated credential page', () => {
-  //   // TODO: add api method for quick credential creation
-  //   // Adding credential
-  //   cy.createCredential('aws-credential.yaml')
-  //   cy.visit('/multicloud/credentials')
-  //   cy.get('button').contains('Add credential').should('have.attr', 'aria-disabled', 'false').click()
-  //   cy.location('href').should('include', '/credentials/create')
-  // })
+  it('CLC: Traverse to create credential page from populated credential page', () => {
+    // TODO: add api method for quick credential creation
+    // Adding credential
+    cy.createCredential('aws-credential.yaml')
+    cy.visit('/multicloud/credentials')
+    cy.get('button').contains('Add credential').should('have.attr', 'aria-disabled', 'false').click()
+    cy.location('href').should('include', '/credentials/create')
+  })
 
   it('RHACM4K-567: CLC: Create AWS provider connections', { tags: ['aws', 'credentials'] }, () => {
     cy.visit('/multicloud/credentials/create')
