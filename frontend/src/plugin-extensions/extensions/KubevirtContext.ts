4c7c656aa8a2f1fa025feac31f3d56b5078b7fd4
Randy Bruno Piverger <rbrunopi@redhat.com>
2024-12-06 11:38:29 -0800

    KubevirtPluginWrapper and context provider
    Signed-off-by: Randy Bruno Piverger <rbrunopi@redhat.com>

--- frontend/src/plugin-extensions/extensions/KubevirtContext.ts
+++ frontend/src/plugin-extensions/extensions/KubevirtContext.ts
@@ -4,20 +4,18 @@ import * as OpenshiftDynamicPluginSDK from '@openshift-console/dynamic-plugin-sd
 import { Context } from 'react'
 
 /** Properties type */
-export type KubevirtPluginData = {
+export type KubevirtPluginContext = {
   dynamicPluginSDK: typeof OpenshiftDynamicPluginSDK
 }
 
-export type SearchDetailsProps = {
-  component: CodeRef<React.ComponentType>
-  context?: CodeRef<Context<KubevirtPluginData>>
+export type KubevirtPluginDataProps = {
+  context?: CodeRef<Context<KubevirtPluginContext>>
 }
 
 /** This extension allows plugins to contribute a tab to Overview page */
-export type SearchDetails = ExtensionDeclaration<'acm.search/details', SearchDetailsProps>
+export type KubevirtPluginContextExtension = ExtensionDeclaration<'acm.kubevirt-context', KubevirtPluginDataProps>
 
 // Type guards
-
-export const isSearchDetails = (e: Extension): e is SearchDetails => {
-  return e.type === 'acm.search/details'
+export const isKubevirtPluginContext = (e: Extension): e is KubevirtPluginContextExtension => {
+  return e.type === 'acm.kubevirt-context'
 }
---
+++ frontend/src/routes/Search/Details/KubevirtPluginWrapper.tsx
@@ -0,0 +1,24 @@
+/* Copyright Contributors to the Open Cluster Management project */
+import React, { useContext, createContext, Context } from 'react'
+import { KubevirtPluginData } from '../../../plugin-extensions/extensions/SearchDetails'
+import { PluginContext } from '../../../lib/PluginContext'
+
+// Define a default context
+const DefaultKubevirtPluginContext = createContext<KubevirtPluginData>({} as KubevirtPluginData)
+
+const KubevirtPluginWrapper = (props: { children: React.ReactNode }) => {
+  const { children } = props
+  const { acmExtensions } = useContext(PluginContext)
+  let KubevirtPluginContext = DefaultKubevirtPluginContext
+
+  if (acmExtensions?.kubevirtContext && acmExtensions.kubevirtContext.length) {
+    KubevirtPluginContext = acmExtensions.kubevirtContext[0].properties.context as Context<KubevirtPluginData>
+  }
+
+  const kubevirtContext = useContext(KubevirtPluginContext)
+  const contextOverride = { ...kubevirtContext, dynamicPluginSDK: { ...kubevirtContext.dynamicPluginSDK } }
+
+  return <KubevirtPluginContext.Provider value={contextOverride}>{children}</KubevirtPluginContext.Provider>
+}
+
+export default KubevirtPluginWrapper
